54636c245bb5a820d437bafea322f3d6d8068ca3
AkiraXie <l997460364@outlook.com>
2020-06-20 00:40:44 +0800

    fix and optimize xsetu

--- hoshino/modules/custom/xsetu.py
+++ hoshino/modules/custom/xsetu.py
@@ -1,19 +1,29 @@
 from hoshino import R,CommandSession,Service,Privilege as Priv
 import base64
 from hoshino import aiorequests
+from hoshino.util import FreqLimiter, DailyNumberLimiter
 sv=Service('xsetu',visible=False,enable_on_default=False,manage_priv=Priv.SUPERUSER)
-
+_nlmt = DailyNumberLimiter(10)
+_flmt = FreqLimiter(15)
 
 
 
 async def getsetu():
-    resp=await aiorequests.get('https://api.loli.com.se')
+    resp=await aiorequests.get('https://api.loli.com.se',timeout=5)
     img=base64.b64encode(await resp.content).decode()
     return f'[CQ:image,cache=0,file=base64://{img}]'
 
 
 @sv.on_rex(r'^(不够[涩瑟色]|[涩瑟色]图|来一?[点份张].*[涩瑟色]|再来[点份张]|看过了|铜)', normalize=True)
 async def pushsetu(bot, ctx, match):
+    if not _nlmt.check(uid):
+        await bot.send(ctx, EXCEED_NOTICE, at_sender=True)
+        return
+    if not _flmt.check(uid):
+        await bot.send(ctx, '您冲得太快了，请稍候再冲', at_sender=True)
+        return
+    _flmt.start_cd(uid)
+    _nlmt.increase(uid)
     try:
         msg=await getsetu()
         await bot.send(ctx,msg)
