5db781c9cea655913253d0fb24d1961bc0d823e3
AkiraXie <l997460364@outlook.com>
2020-07-24 22:55:39 +0800

    update setu sources

--- hoshino/modules/custom/ysetu/__init__.py
+++ hoshino/modules/custom/ysetu/__init__.py
@@ -1,6 +1,8 @@
 #该色图功能取自tngsohack的色图api，十分感谢
 from hoshino import Service, Privilege as Priv
 import os
+import requests
+import base64
 import random
 from hoshino.util import FreqLimiter, DailyNumberLimiter
 sv = Service('ysetu',visible=False)
@@ -8,25 +10,25 @@ _nlmt = DailyNumberLimiter(15)
 _flmt = FreqLimiter(15)
 
 
-
 def getsetu():
-    return f'[CQ:image,cache=0,url=https://api.photo.lolicon.plus/stv2/]'
+    urls=['https://api.photo.lolicon.plus/stv2/','http://www.dmoe.cc/random.php','https://s0.xinger.ink/acgimg/acgurl.php','https://img.ijglb.com/api.php?action=pc']
+    sturl=random.choice(urls)
+    resp=requests.get(sturl,timeout=5)
+    img=base64.b64encode(resp.content).decode()
+    return f'[CQ:image,cache=0,file=base64://{img}]'
 
 
-@sv.on_rex(r'^(不够[涩瑟色]|[涩瑟色]图|来一?[点份张].*[涩瑟色]|再来[点份张]|铜)', normalize=True)
-async def pushsetu(bot, ctx, match):
-    uid = ctx['user_id']
+@sv.on_command('色图',aliases=('铜','不够色','不够涩','瑟图','涩图'))
+async def pushsetu(session):
+    uid = session.ctx['user_id']
     if not _nlmt.check(uid):
-        await bot.send(ctx, "您今天已经冲了15次了,请明天再来！", at_sender=True)
-        return
+        session.finish( "您今天已经冲了15次了,请明天再来！", at_sender=True)
     if not _flmt.check(uid):
-        await bot.send(ctx, '您冲得太快了，请稍候再冲', at_sender=True)
-        return
+        session.finish('您冲得太快了，请稍候再冲', at_sender=True)
     _flmt.start_cd(uid)
     try:
         msg = getsetu()
-        await bot.send(ctx, msg)
+        await session.send(msg)
         _nlmt.increase(uid)
     except:
-        await bot.send(ctx, '它太色了，被吃掉了QAQ')
-        return
+        session.finish('它太色了,被吃掉了qaq', at_sender=True)
