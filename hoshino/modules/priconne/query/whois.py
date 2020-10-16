b16a80cc324bf399ad9939b7bd87a86caa731147
AkiraXie <l997460364@outlook.com>
2020-10-16 23:46:38 +0800

    fix:optimze lookupcard in whois

--- hoshino/modules/priconne/query/whois.py
+++ hoshino/modules/priconne/query/whois.py
@@ -24,8 +24,9 @@ async def whois(bot, ctx, match):
 
     msg = f'\n{chara.name}\n{chara.icon.cqcode}'
     await bot.send(ctx, msg, at_sender=True)
-@sv.on_rex(r'^[看查]?\s?([1-6]星)?\s?(.{1,20})(立绘|卡面)$')
+@sv.on_rex(r'^[看查]?\s?([1-6一二三四五六]星)?\s?(.{1,20})(立绘|卡面)$')
 async def lookcard(bot,ctx,match):
+    worddic={"一":1,"二":2,"三":3,"四":4,"五":5,"六":6}
     uid = ctx['user_id']
     if not _lmt1.check(uid):
         await bot.send(ctx, '您查询得太快了，请稍等一会儿', at_sender=True)
@@ -33,6 +34,7 @@ async def lookcard(bot,ctx,match):
     _lmt1.start_cd(uid)
     name = match.group(2)
     star=match.group(1)[0] if match.group(1) else 0
+    star=worddic.get(star,star)
     chara = Chara.fromname(name, star=int(star))
     if chara.id == Chara.UNKNOWN:
         msg=[f'兰德索尔似乎没有叫"{name}"的人']
