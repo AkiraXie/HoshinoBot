0316fb3419c8375d2ee4c833a041795728fd4cef
AkiraXie <l997460364@outlook.com>
2020-12-01 19:57:48 +0800

    fix bug

--- hoshino/modules/custom/gift.py
+++ hoshino/modules/custom/gift.py
@@ -3,7 +3,7 @@ from random import randint
 @sucmd('gift',aliases=('礼物','送礼'),force_private=False)
 async def gift(session):
     msg=session.ctx['message']
-    for m in ctx['message']:
+    for m in msg:
         if m.type == 'at' and m.data['qq'] != 'all' :
             qq = m.data['qq']
             break
